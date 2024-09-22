31e10297aa57b491fa1c17bdbcee5540a588296b
Rajkamalcode <srajkamal717@gmail.com>
2024-09-22 18:13:04 +0530

    added netlify

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bfhlRoutes = require('./src/routes/bfhlRoutes');
 const cors = require('cors');
+const serverless = require("serverless-http");
 
 
 const app = express();
@@ -14,3 +15,6 @@ app.use('api/bfhl', bfhlRoutes);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+export const handler = serverless(app);
+
